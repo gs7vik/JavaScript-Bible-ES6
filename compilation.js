cc066c4cd8933a631217945355601d1c4ecc87b5
Satvik G S <1gssatvik@gmail.com>
2020-12-24 09:11:31 +0530

    new two changes

--- compilation.js
+++ compilation.js
@@ -1,21 +1,24 @@
 /* COMPILATION PHASE
 Scopes:
 Global {
+  Line 2:
+  const b; declared and unitiliased and located in TDZ
   Line 16: 
   var a; decleared and initialized with "undefined"
-
+ 
 }
 sum {
-
+sum declared but uninitialised
 }
 
 if {
+ multresult declared but uniniti...
 
 } else {
-
+ sumresult declared but uninitia...
 }
 
 mult {
-
+ declared,initialised and assigned
 }
 */
--- execution.js
+++ execution.js
@@ -4,8 +4,13 @@ Global scope variable "a" becomes assigned.
 Value of the "a" is 2.
 
 Line 2:
+b becomes assigned and has the value 5
 
-Line 4: 
+Line 4:
+sum is initialised and assigned. 
+
+
+line 8:
 
 ...
 */
